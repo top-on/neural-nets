5856fad324818bd179ebf1d6bde0fbd2cd164041
Thorben Jensen <jensen.thorben@gmail.com>
2017-10-10 11:12:51 +0200

    results from zarathustra.

--- src/lstm_text_generation.py
+++ src/lstm_text_generation.py
@@ -103,7 +103,8 @@ def generate_text(model):
 
 
 # selecting iterations to generate text for
-get_text = []  # [1, 5, 10, 15, 20]
+# get_text = [1, 5, 10, 15, 20]
+get_text = [10, 20, 40, 60, 80]
 
 # train the model, output generated text after each iteration
 for i in range(1, 81):
