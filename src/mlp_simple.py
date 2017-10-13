ac4f1b81a9ea3544fcc40cf88a587b90a816ee17
Thorben Jensen <jensen.thorben@gmail.com>
2017-10-13 11:11:54 +0200

    finalizing example of simple perceptron

--- src/mlp_simple.py
+++ src/mlp_simple.py
@@ -2,7 +2,7 @@
 
 from keras.layers import Dense
 from keras.models import Sequential
-from keras.optimizers import Adam
+from keras.optimizers import SGD
 import numpy as np
 import os
 import random
@@ -12,19 +12,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # define model
 model = Sequential()
-model.add(Dense(1, input_dim=3, kernel_initializer='normal', use_bias=False))
-model.compile(loss='mean_absolute_error', optimizer=Adam(lr=0.1))
+model.add(Dense(1, input_dim=2, kernel_initializer='normal', use_bias=False))
+model.compile(loss='mean_absolute_error', optimizer=SGD(lr=0.01))
 
 # set initial weights
 weights = model.get_weights()
-weights[0] = np.array([[.5], [.5], [.5]])
+weights[0] = np.array([[.5], [.5]])
 model.set_weights(weights=weights)
 
 # check initial weights
 model.get_weights()[0].tolist()
 
-# train with 1 observation
-for i in range(0, 100):
-    x = np.array([[1, random.random(), random.random()]])
+# train with 1 observation each
+for i in range(0, 1000):
+    # print weights
+    print(model.get_weights()[0].tolist())
+    # train on one example
+    x = np.array([[1, random.random()]])
     model.fit(x=x, y=np.array([1]), batch_size=1, epochs=1, verbose=0)
-model.get_weights()[0].tolist()
