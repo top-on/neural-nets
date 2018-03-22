3aa48fdb535dff860de673e0da549280e12ebc78
Thorben Jensen <tjensen@informationsfabrik.de>
2018-03-22 16:18:02 +0100

    first working version of image classifier

--- src/transfer_learning/transfer_learning.py
+++ src/transfer_learning/transfer_learning.py
@@ -10,6 +10,7 @@ from keras.layers import Dense
 import numpy as np
 import PIL
 
+
 def build_model() -> Model:
     """Build Keras model, based on ResNet50, trained on imagenet."""
     # download link: https://github.com/fchollet/deep-learning-models/releases/
@@ -33,31 +34,57 @@ def build_model() -> Model:
                         metrics=["accuracy"])
     return model_final
 
+
 def preprocess_image(img_path: PIL.Image.Image) -> np.ndarray:
     """Preprocess image for ResNet50."""
     img = load_img(img_path, target_size=(224, 224))
     img = img_to_array(img)
-    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
-    img = preprocess_input(img)
+    # img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
     return img
+
 
 if __name__ == '__main__':
     # build model
     model = build_model()
 
-    # path to image
+    # training data
+    images = np.array([])
+    labels = []
+    label_encoding = {'bike': np.array([[[1, 0]]]),
+                      'car': np.array([[[0, 1]]])}
+
+    # add bike images
     path = 'data/images/bicycle tire profile closeup -car/'
     files = os.listdir(path)
-    image_path = path + files[7]
+    bike_images = [preprocess_image(path + file) for file in files]
+    bike_images = np.array(bike_images)
+    bike_labels = [label_encoding['bike'] for i in range(len(bike_images))]
+    bike_labels = np.array(bike_labels)
 
-    # load image
-    image = preprocess_image(image_path)
+    # add car images (TODO: DRY pattern)
+    path = 'data/images/car tire profile closeup -bicycle/'
+    files = os.listdir(path)
+    car_images = [preprocess_image(path + file) for file in files]
+    car_images = np.array(car_images)
+    car_labels = [label_encoding['car'] for i in range(len(car_images))]
+    car_labels = np.array(car_labels)
+
+    # combine numpy arrays
+    images = np.concatenate((bike_images, car_images), axis=0)
+    labels = np.concatenate((bike_labels, car_labels), axis=0)
 
     # fit on one image
-    labels = {'bike': 0,
-              'car': 1}
-    model.fit(x=image, y=np.array([[[[1, 0]]]]), epochs=10)
+    images = preprocess_input(images, mode='tf')
+    history = model.fit(x=np.array(images),
+                        y=np.array(labels),
+                        epochs=3,
+                        validation_split=0.2,
+                        verbose=2,
+                        batch_size=20)
+    print(history.history)
 
     # predict
-    prediction = model.predict(image)
+    img = images[-1]
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    prediction = model.predict(img)
     print('Prediction for trained image: {}'.format(prediction))
